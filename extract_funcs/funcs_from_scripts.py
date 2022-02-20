fee58660d283d0cf2d5fbfc2087e434a8294cf3b
laundmo <laurinschmidt2001@gmail.com>
2022-02-20 18:26:36 +0100

    gather functions from vscripts in a json

---
+++ extract_funcs/funcs_from_scripts.py
@@ -0,0 +1,118 @@
+from collections import defaultdict
+import json
+from pathlib import Path
+import re
+
+from matplotlib.font_manager import json_dump
+
+NS_MODS = Path("PATH_TO/R2Northstar/mods/")
+
+# Layers of vscripts, topmost is overwritten by below
+VSCRIPT_LAYERS = [
+    (
+        "Titanfall 2",
+        Path("PATH/TO/assets_dump/scripts/vscripts"),
+    ),
+    (
+        "Northstar.CustomServers",
+        NS_MODS / "Northstar.CustomServers/mod/scripts/vscripts",
+    ),
+    ("Northstar.Coop", NS_MODS / "Northstar.Coop/mod/scripts/vscripts"),
+    ("Northstar.Custom", NS_MODS / "Northstar.Custom/mod/scripts/vscripts"),
+    ("Northstar.Client", NS_MODS / "Northstar.Client/mod/scripts/vscripts"),
+]
+
+
+def get_layer(filename):
+    for name, layer in reversed(VSCRIPT_LAYERS):
+        path = layer / filename
+        if path.exists():
+            return path, name
+
+
+def openl(filename: str, mode="r", encoding="utf-8"):
+    path, name = get_layer(filename)
+    if path:
+        return name, path.open(mode, encoding=encoding, errors="ignore")
+    raise FileNotFoundError(f"Could not locate {filename} in any layer.")
+
+
+with open("scripts.json") as f:
+    scripts = json.load(f)
+
+sl_cmnt = re.compile(r"\/\/.*")
+funname_re = re.compile(r"function (\w+)\(")
+
+
+def runon_parse(inp: str):
+    try:
+        inp, additional = inp.split("&&")
+        additional = " &&" + additional
+    except ValueError:
+        additional = ""
+    runons = inp.split("||")
+    runons = [r.strip(" \n()") + additional for r in runons]
+    return runons
+
+
+funcs = defaultdict(list)
+
+for runon, filelist in scripts.items():
+    runons = runon_parse(runon)
+    if "CONSOLE_PROG" in runon:
+        continue
+    for filename in filelist:
+        try:
+            found_in, fp = openl(filename)
+            with fp as f:
+                ml_comment_depth = 0
+                global_all = False
+                runons_mod = []
+                for line in f:
+                    line = line.removesuffix("\n")
+                    # globalize all
+                    if "globalize_all_functions" in line:
+                        global_all = True
+
+                    # comment skipping
+                    line = sl_cmnt.sub("", line)
+                    if "/*" in line:
+                        ml_comment_depth += 1
+                        line = line.split("/*")[0]
+                    elif "*/" in line:
+                        ml_comment_depth -= 1
+                        line = line.split("*/")[1]
+                    elif ml_comment_depth > 0:
+                        continue
+
+                    # preproc conditions
+                    if "#if" in line:
+                        runon_modifier = line.strip().replace("#if ", "")
+                        runons_mod = runon_parse(runon_modifier)
+                    if "#elseif" in line:
+                        runon_modifier = line.strip().replace("#elseif ", "")
+                        runons_mod = runon_parse(runon_modifier)
+                    if "#endif" in line:
+                        runon_modifier = ""
+                        runons_mod = []
+
+                    # function
+                    match = funname_re.search(line)
+                    if match:
+                        name = match.group(1)
+                        local_params = {}
+                        local_params["name"] = name
+                        local_params["found_in"] = found_in
+                        local_params["file"] = filename
+                        if "global function" in line:
+                            local_params["global"] = True
+                        else:
+                            local_params["global"] = global_all
+                        local_params["line"] = line
+                        runon = runons_mod or runons
+                        funcs[str(runon)].append(local_params)
+        except FileNotFoundError:
+            print(filename)
+
+with open("funcs.json", "w") as f:
+    json.dump(funcs, f)
